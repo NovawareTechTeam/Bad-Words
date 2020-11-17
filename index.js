1d341615cabe2acbef2e6e9533fe05787351e1e7
NovawareTechTeam <74027744+NovawareTechTeam@users.noreply.github.com>
2020-11-16 20:59:05 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Go to Bad Words/words.json to view the array of words.\n');
+console.log('To create a file in your language, go to Bad Words/other languages\nand create a file named "(your language) words.json."');
